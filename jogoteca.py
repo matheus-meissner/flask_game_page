229b4d24d322ae13d399d3f65f95316582db30ee
Matheus Meissner <matheus.iembo@gmail.com>
2024-08-29 16:20:38 -0300

    cria_pag_criar_metodo_post

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template #Importa função flask da biblioteca flask
+from flask import Flask, render_template, request #Importa função flask da biblioteca flask
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,18 +6,30 @@ class Jogo:
         self.categoria=categoria
         self.console=console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__) #Atribui a aplicação para a variável app
 
-@app.route('/inicio') #Cria nova rota
-def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/') #Cria nova rota
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista) #Importa HTML, define variável
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo Jogo')
 
-app.run()
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+app.run(debug=True)
