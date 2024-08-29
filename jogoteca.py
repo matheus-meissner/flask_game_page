3c56ac5e7002a9b169ed352ea2367eb836d181a9
Matheus Meissner <matheus.iembo@gmail.com>
2024-08-29 15:20:31 -0300

    importa_html_flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,9 @@
-from flask import Flask #Importa função flask da biblioteca flask
+from flask import Flask, render_template #Importa função flask da biblioteca flask
 
 app = Flask(__name__) #Atribui a aplicação para a variável app
 
 @app.route('/inicio') #Cria nova rota
 def ola():
-    return '<h1>Olá Mundo!</h1>'
+    return render_template('lista.html') #Importa HTML
 
 app.run()
